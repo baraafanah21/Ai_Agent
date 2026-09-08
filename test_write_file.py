b37a61423f470fc359805a588e52d9fb22694e4a
Bara Afanah <baraafanah1@gmail.com>
2026-09-08 16:22:35 +0300

    Add write_file tests

---
+++ test_write_file.py
@@ -0,0 +1,18 @@
+from functions.write_file import write_file
+
+
+def main() -> None:
+    print("Result for 'lorem.txt':")
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print()
+
+    print("Result for 'pkg/morelorem.txt':")
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print()
+
+    print("Result for '/tmp/temp.txt':")
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+
+if __name__ == "__main__":
+    main()
